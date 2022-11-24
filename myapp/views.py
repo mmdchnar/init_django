816b5c1d22ad8259f0641f26d22573dc6470f94b
mmdchnar <mmdchnar@gmail.com>
2022-11-24 22:29:48 +0330

    Add some example and comment

--- myapp/views.py
+++ myapp/views.py
@@ -1,11 +1,27 @@
 from django.shortcuts import render
-# from django.http import HttpResponse, HttpResponseRedirect
-# from django.views.generic.base import RedirectView
+from django.urls import reverse
+from django.http import HttpResponseRedirect #, HttpResponse
 
 
-def index(req):
-    return render(req, 'index.html')
+
+def index(request):
+    name = request.COOKIES.get('name')
+
+    if not name:
+        # do something
+        pass
+
+    if request.method == 'POST':
+        if 'name' in dict(request.POST.items()):
+            if request.POST['name'].replace(' ', '') != '':
+                response = HttpResponseRedirect(reverse('/'))
+                response.set_cookie('name', request.POST['name'])
+                # response.delete_cookie('name')
+                return response
+       
+
+    return render(request, 'index.html')
 
 
-def _404(req, ex = None):
-    return render(req, '404.html')
+def _404(request, ex = None):
+    return render(request, '404.html')
