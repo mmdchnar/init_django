3a5b47ffc2ecff954a1def0a0fe0436f3661a2ff
mmdchnar <mmdchnar@gmail.com>
2022-11-24 20:26:21 +0330

    Add * to ALLOWED_HOSTS

--- mysite/settings.py
+++ mysite/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-2yj57(($d!9+$ruczg$)z6+64-rnvxwg(kv-kdv1!k(a_ij(=i
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
